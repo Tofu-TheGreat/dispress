c1bd3778f659ae761bb2f2ce1391b5cef654b7c4
Your Name <you@example.com>
2023-10-25 12:15:51 +0700

    Add basic dashboard template

---
+++ resources/views/admin/pages/layout.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>Ecommerce Dashboard &mdash; Stisla</title>
+
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="assets/modules/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/modules/fontawesome/css/all.min.css">
+
+    <!-- CSS Libraries -->
+    <link rel="stylesheet" href="assets/modules/jqvmap/dist/jqvmap.min.css">
+    <link rel="stylesheet" href="assets/modules/summernote/summernote-bs4.css">
+    <link rel="stylesheet" href="assets/modules/owlcarousel2/dist/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/modules/owlcarousel2/dist/assets/owl.theme.default.min.css">
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/components.css">
+    <!-- Start GA -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-94034622-3');
+    </script>
+    <!-- /END GA -->
+</head>
+
+<body>
+    <div id="app">
+        <div class="main-wrapper main-wrapper-1">
+            <div class="navbar-bg"></div>
+            {{-- NAVBAR --}}
+            @include('admin.partial.navbar')
+            {{-- SIDEBAR --}}
+            @include('admin.partial.sidebar')
+
+            <!-- Main Content -->
+            @yield('main')
+            {{-- FOOTEE --}}
+            @include('admin.partial.footer')
+        </div>
+    </div>
+
+    <!-- General JS Scripts -->
+    <script src="assets/modules/jquery.min.js"></script>
+    <script src="assets/modules/popper.js"></script>
+    <script src="assets/modules/tooltip.js"></script>
+    <script src="assets/modules/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
+    <script src="assets/modules/moment.min.js"></script>
+    <script src="assets/js/stisla.js"></script>
+
+    <!-- JS Libraies -->
+    <script src="assets/modules/jquery.sparkline.min.js"></script>
+    <script src="assets/modules/chart.min.js"></script>
+    <script src="assets/modules/owlcarousel2/dist/owl.carousel.min.js"></script>
+    <script src="assets/modules/summernote/summernote-bs4.js"></script>
+    <script src="assets/modules/chocolat/dist/js/jquery.chocolat.min.js"></script>
+
+    <!-- Page Specific JS File -->
+    <script src="assets/js/page/index.js"></script>
+
+    <!-- Template JS File -->
+    <script src="assets/js/scripts.js"></script>
+    <script src="assets/js/custom.js"></script>
+</body>
+
+</html>
---
+++ resources/views/admin/partial/sidebar.blade.php
@@ -0,0 +1,56 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">Stisla</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+            <li class="dropdown active">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="index-0.html">General Dashboard</a></li>
+                    <li class=active><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
+                </ul>
+            </li>
+            <li class="menu-header">Starter</li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
+                    <span>Layout</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="layout-default.html">Default Layout</a></li>
+                    <li><a class="nav-link" href="layout-transparent.html">Transparent Sidebar</a></li>
+                    <li><a class="nav-link" href="layout-top-navigation.html">Top Navigation</a></li>
+                </ul>
+            </li>
+            <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i> <span>Bootstrap</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="bootstrap-alert.html">Alert</a></li>
+                    <li><a class="nav-link" href="bootstrap-badge.html">Badge</a></li>
+                    <li><a class="nav-link" href="bootstrap-breadcrumb.html">Breadcrumb</a></li>
+                    <li><a class="nav-link" href="bootstrap-buttons.html">Buttons</a></li>
+                    <li><a class="nav-link" href="bootstrap-card.html">Card</a></li>
+                    <li><a class="nav-link" href="bootstrap-carousel.html">Carousel</a></li>
+                    <li><a class="nav-link" href="bootstrap-collapse.html">Collapse</a></li>
+                    <li><a class="nav-link" href="bootstrap-dropdown.html">Dropdown</a></li>
+                    <li><a class="nav-link" href="bootstrap-form.html">Form</a></li>
+                    <li><a class="nav-link" href="bootstrap-list-group.html">List Group</a></li>
+                    <li><a class="nav-link" href="bootstrap-media-object.html">Media Object</a></li>
+                    <li><a class="nav-link" href="bootstrap-modal.html">Modal</a></li>
+                    <li><a class="nav-link" href="bootstrap-nav.html">Nav</a></li>
+                    <li><a class="nav-link" href="bootstrap-navbar.html">Navbar</a></li>
+                    <li><a class="nav-link" href="bootstrap-pagination.html">Pagination</a></li>
+                    <li><a class="nav-link" href="bootstrap-popover.html">Popover</a></li>
+                    <li><a class="nav-link" href="bootstrap-progress.html">Progress</a></li>
+                    <li><a class="nav-link" href="bootstrap-table.html">Table</a></li>
+                    <li><a class="nav-link" href="bootstrap-tooltip.html">Tooltip</a></li>
+                    <li><a class="nav-link" href="bootstrap-typography.html">Typography</a></li>
+                </ul>
+            </li>
+        </ul>
+    </aside>
+</div>

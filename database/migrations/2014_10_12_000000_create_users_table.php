3457f05c4a8e10451ea5749ce0c99df993d80fe3
fadli154 <fadlihifziansyah154@gmail.com>
2023-10-26 08:47:31 +0700

    Add new field nomor_telpon and lenght

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->enum('jabatan', ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']);
             $table->string('username', 60);
             $table->string('email')->unique();
+            $table->string('nomor_telpon', 15)->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();

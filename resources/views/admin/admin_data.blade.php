23371583514ceead8c43690cff3d093a533f620d
fadli154 <fadlihifziansyah154@gmail.com>
2023-11-01 13:20:03 +0700

    Change Import Modal centered

--- resources/views/admin/admin_data.blade.php
+++ resources/views/admin/admin_data.blade.php
@@ -87,7 +87,7 @@
 
     <!-- Modal Import -->
     <div class="modal fade" id="importmodal" aria-labelledby="importmodalLabel" aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal-dialog  modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="importmodalLabel">Modal Import Users</h5>

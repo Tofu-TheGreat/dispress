ce575b49e57cf9b234b2bc951869ae76e3da92e6
Your Name <you@example.com>
2023-11-05 18:34:40 +0700

    changes routes for import

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\ExportController;
+use App\Http\Controllers\ImportController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,8 +31,8 @@ Route::get('/dashboard', [Controller::class, 'dashboard'])->middleware('auth', '
 Route::post('/login', [LoginController::class, 'login'])->name('login')->middleware('guest');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-Route::get('admin-export', [ExportController::class, 'export'])->name('admin.export');
-Route::post('admin-import', [ExportController::class, 'import'])->name('admin.import');
+Route::get('admin-export', [ExportController::class, 'export_admin'])->name('admin.export');
+Route::post('admin-import', [ImportController::class, 'import_admin'])->name('admin.import');
 
 Route::resource('/admin', AdminController::class)->middleware('auth');
 Route::post("/admin-index", [AdminController::class, "indexAdmin"]);

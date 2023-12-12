c67672a1c18ad16d1c2500d52129eded6f7e847a
fadli154 <fadlihifziansyah154@gmail.com>
2023-12-12 21:22:56 +0700

    Fiture: Add unfinished chart pengajuan vs disposisi

--- resources/views/admin/pages/dashboard-admin.blade.php
+++ resources/views/admin/pages/dashboard-admin.blade.php
@@ -74,12 +74,165 @@
                 </div>
             </div>
         </div>
+        <div class="row">
+            <div class="col-lg-8">
+                <div class="card">
+                    <div class="card-header">
+                        <span data-toggle="tooltip" data-placement="top"
+                            title="Ini adalah data Pengajuan dan Disposisi yang dilakukan kamu."
+                            data-original-title="Ini adalah data Pengajuan dan Disposisi yang dilakukan kamu." disabled>
+                            <i class="bi bi-question-circle mr-2"></i>
+                        </span>
+                        <h4>Pengajuan vs Disposisi</h4>
+                    </div>
+                    <div class="card-body">
+                        <canvas id="myChart" height="158"></canvas>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card gradient-bottom">
+                    <div class="card-header">
+                        <span data-toggle="tooltip" data-placement="top" title="Ini adalah data Pengajuan terbaru"
+                            data-original-title="Ini adalah data Pengajuan terbaru" disabled>
+                            <i class="bi bi-question-circle mr-2"></i>
+                        </span>
+                        <h4>Pengajuan Terbaru</h4>
+                        <div class="card-header-action dropdown">
+                            <a href="#" data-toggle="dropdown" class="btn btn-danger dropdown-toggle">Month</a>
+                            <ul class="dropdown-menu dropdown-menu-sm dropdown-menu-right">
+                                <li class="dropdown-title">Select Period</li>
+                                <li><a href="#" class="dropdown-item">Today</a></li>
+                                <li><a href="#" class="dropdown-item">Week</a></li>
+                                <li><a href="#" class="dropdown-item active">Month</a></li>
+                                <li><a href="#" class="dropdown-item">This Year</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="card-body" id="top-5-scroll">
+                        <ul class="list-unstyled list-unstyled-border">
+                            <li class="media">
+                                <img class="mr-3 rounded" width="55" src="assets/img/products/product-3-50.png"
+                                    alt="product">
+                                <div class="media-body">
+                                    <div class="float-right">
+                                        <div class="font-weight-600 text-muted text-small">86 Sales</div>
+                                    </div>
+                                    <div class="media-title">oPhone S9 Limited</div>
+                                    <div class="mt-1">
+                                        <div class="budget-price">
+                                            <div class="budget-price-square bg-primary" data-width="64%"></div>
+                                            <div class="budget-price-label">$68,714</div>
+                                        </div>
+                                        <div class="budget-price">
+                                            <div class="budget-price-square bg-danger" data-width="43%"></div>
+                                            <div class="budget-price-label">$38,700</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="media">
+                                <img class="mr-3 rounded" width="55" src="assets/img/products/product-4-50.png"
+                                    alt="product">
+                                <div class="media-body">
+                                    <div class="float-right">
+                                        <div class="font-weight-600 text-muted text-small">67 Sales</div>
+                                    </div>
+                                    <div class="media-title">iBook Pro 2018</div>
+                                    <div class="mt-1">
+                                        <div class="budget-price">
+                                            <div class="budget-price-square bg-primary" data-width="84%"></div>
+                                            <div class="budget-price-label">$107,133</div>
+                                        </div>
+                                        <div class="budget-price">
+                                            <div class="budget-price-square bg-danger" data-width="60%"></div>
+                                            <div class="budget-price-label">$91,455</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="media">
+                                <img class="mr-3 rounded" width="55" src="assets/img/products/product-1-50.png"
+                                    alt="product">
+                                <div class="media-body">
+                                    <div class="float-right">
+                                        <div class="font-weight-600 text-muted text-small">63 Sales</div>
+                                    </div>
+                                    <div class="media-title">Headphone Blitz</div>
+                                    <div class="mt-1">
+                                        <div class="budget-price">
+                                            <div class="budget-price-square bg-primary" data-width="34%"></div>
+                                            <div class="budget-price-label">$3,717</div>
+                                        </div>
+                                        <div class="budget-price">
+                                            <div class="budget-price-square bg-danger" data-width="28%"></div>
+                                            <div class="budget-price-label">$2,835</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="media">
+                                <img class="mr-3 rounded" width="55" src="assets/img/products/product-3-50.png"
+                                    alt="product">
+                                <div class="media-body">
+                                    <div class="float-right">
+                                        <div class="font-weight-600 text-muted text-small">28 Sales</div>
+                                    </div>
+                                    <div class="media-title">oPhone X Lite</div>
+                                    <div class="mt-1">
+                                        <div class="budget-price">
+                                            <div class="budget-price-square bg-primary" data-width="45%"></div>
+                                            <div class="budget-price-label">$13,972</div>
+                                        </div>
+                                        <div class="budget-price">
+                                            <div class="budget-price-square bg-danger" data-width="30%"></div>
+                                            <div class="budget-price-label">$9,660</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="media">
+                                <img class="mr-3 rounded" width="55" src="assets/img/products/product-5-50.png"
+                                    alt="product">
+                                <div class="media-body">
+                                    <div class="float-right">
+                                        <div class="font-weight-600 text-muted text-small">19 Sales</div>
+                                    </div>
+                                    <div class="media-title">Old Camera</div>
+                                    <div class="mt-1">
+                                        <div class="budget-price">
+                                            <div class="budget-price-square bg-primary" data-width="35%"></div>
+                                            <div class="budget-price-label">$7,391</div>
+                                        </div>
+                                        <div class="budget-price">
+                                            <div class="budget-price-square bg-danger" data-width="28%"></div>
+                                            <div class="budget-price-label">$5,472</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="card-footer pt-3 d-flex justify-content-center">
+                        <div class="budget-price justify-content-center">
+                            <div class="budget-price-square bg-primary" data-width="20"></div>
+                            <div class="budget-price-label">Selling Price</div>
+                        </div>
+                        <div class="budget-price justify-content-center">
+                            <div class="budget-price-square bg-danger" data-width="20"></div>
+                            <div class="budget-price-label">Budget Price</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 @endsection
 
 @section('script')
     <script src="{{ asset('assets/modules/chart.min.js') }}"></script>
 
+    {{-- Chart Disposisi --}}
     <script>
         const disposisi_chart = document.getElementById("disposisi-chart").getContext('2d');
 
@@ -138,6 +291,8 @@
             }
         });
     </script>
+
+    {{-- Chart Pengajuan --}}
     <script>
         const pengajuan_chart = document.getElementById("pengajuan-chart").getContext('2d');
 
@@ -190,6 +345,69 @@
                         },
                         ticks: {
                             display: false
+                        }
+                    }]
+                },
+            }
+        });
+    </script>
+
+    {{-- Chart data pengajuan dan disposisi yang di lakukan oleh admin ini --}}
+    <script>
+        var ctx = document.getElementById("myChart").getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: ["January", "February", "March", "April", "May", "June", "July", "August"],
+                datasets: [{
+                        label: 'Sales',
+                        data: [3200, 1800, 4305, 3022, 6310, 5120, 5880, 6154],
+                        borderWidth: 2,
+                        backgroundColor: 'rgba(63,82,227,.8)',
+                        borderWidth: 0,
+                        borderColor: 'transparent',
+                        pointBorderWidth: 0,
+                        pointRadius: 3.5,
+                        pointBackgroundColor: 'transparent',
+                        pointHoverBackgroundColor: 'rgba(63,82,227,.8)',
+                    },
+                    {
+                        label: 'Budget',
+                        data: [2207, 3403, 2200, 5025, 2302, 4208, 3880, 4880],
+                        borderWidth: 2,
+                        backgroundColor: 'rgba(254,86,83,.7)',
+                        borderWidth: 0,
+                        borderColor: 'transparent',
+                        pointBorderWidth: 0,
+                        pointRadius: 3.5,
+                        pointBackgroundColor: 'transparent',
+                        pointHoverBackgroundColor: 'rgba(254,86,83,.8)',
+                    }
+                ]
+            },
+            options: {
+                legend: {
+                    display: false
+                },
+                scales: {
+                    yAxes: [{
+                        gridLines: {
+                            // display: false,
+                            drawBorder: false,
+                            color: '#f2f2f2',
+                        },
+                        ticks: {
+                            beginAtZero: true,
+                            stepSize: 1500,
+                            callback: function(value, index, values) {
+                                return '$' + value;
+                            }
+                        }
+                    }],
+                    xAxes: [{
+                        gridLines: {
+                            display: false,
+                            tickMarkLength: 15,
                         }
                     }]
                 },

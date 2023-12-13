c88d6e21ba5e3f972a019eb2447df4ff05e3380e
fadli154 <fadlihifziansyah154@gmail.com>
2023-12-13 14:57:30 +0700

    Fiture: Unfinished dashboard staff

--- resources/views/admin/pages/dashboard-staff.blade.php
+++ resources/views/admin/pages/dashboard-staff.blade.php
@@ -4,71 +4,134 @@
 @section('content')
     <section class="section">
         <div class="row">
-            <div class="col-lg-4 col-md-4 col-sm-12">
-                <div class="card card-statistic-2">
-                    <div class="card-stats">
-                        <div class="card-stats-title text-primary">Dashboard Staff
-                        </div>
-                        <div class="card-stats-items">
-                            <div class="card-stats-item">
-                                <div class="card-stats-item-count">24</div>
-                                <div class="card-stats-item-label">Pending</div>
-                            </div>
-                            <div class="card-stats-item">
-                                <div class="card-stats-item-count">12</div>
-                                <div class="card-stats-item-label">Shipping</div>
-                            </div>
-                            <div class="card-stats-item">
-                                <div class="card-stats-item-count">23</div>
-                                <div class="card-stats-item-label">Completed</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card-icon shadow-primary bg-primary">
-                        <i class="fas fa-users"></i>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-primary">
+                        <i class="far fa-user"></i>
                     </div>
                     <div class="card-wrap">
                         <div class="card-header">
                             <h4>Total Staff</h4>
                         </div>
                         <div class="card-body">
-                            {{ $staffCount }}
+                            10
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-4 col-sm-12">
-                <div class="card card-statistic-2">
-                    <div class="card-chart">
-                        <canvas id="balance-chart" height="80"></canvas>
-                    </div>
-                    <div class="card-icon shadow-primary bg-primary">
-                        <i class="fas fa-envelope"></i>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-danger">
+                        <i class="fas fa-envelope-open-text"></i>
                     </div>
                     <div class="card-wrap">
                         <div class="card-header">
-                            <h4>Pengajuan Disposisi</h4>
+                            <h4>Disposisi</h4>
                         </div>
                         <div class="card-body">
-                            $187,13
+                            42
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-4 col-sm-12">
-                <div class="card card-statistic-2">
-                    <div class="card-chart">
-                        <canvas id="sales-chart" height="80"></canvas>
-                    </div>
-                    <div class="card-icon shadow-primary bg-primary">
-                        <i class="fas fa-shopping-bag"></i>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-warning">
+                        <i class="far fa-envelope"></i>
                     </div>
                     <div class="card-wrap">
                         <div class="card-header">
-                            <h4>Sales</h4>
+                            <h4>Surat Keluar</h4>
                         </div>
                         <div class="card-body">
-                            4,732
+                            20
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-success">
+                        <i class="fas fa-circle"></i>
+                    </div>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>Online Users</h4>
+                        </div>
+                        <div class="card-body">
+                            47
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-8">
+                <div class="card">
+                    <div class="card-header">
+                        <span data-toggle="tooltip" data-placement="top"
+                            title="Ini adalah data yang Didisposisikan ke Anda."
+                            data-original-title="Ini adalah data yang Didisposisikan ke Anda." disabled>
+                            <i class="bi bi-question-circle mr-2 text-primary"></i>
+                        </span>
+                        <h4>Disposisi</h4>
+                    </div>
+                    <div class="card-body">
+                        <canvas id="myChart" height="158"></canvas>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card gradient-bottom">
+                    <div class="card-header">
+                        <span data-toggle="tooltip" data-placement="top" title="Ini adalah data disposisi terbaru"
+                            data-original-title="Ini adalah data disposisi terbaru" disabled>
+                            <i class="bi bi-question-circle mr-2 text-primary"></i>
+                        </span>
+                        <h4>Disposisi Terbaru</h4>
+                        <div class="card-header-action dropdown">
+                            <a href="#" data-toggle="dropdown" class="btn btn-danger dropdown-toggle">Month</a>
+                            <ul class="dropdown-menu dropdown-menu-sm dropdown-menu-right">
+                                <li class="dropdown-title">Select Period</li>
+                                <li><a href="#" class="dropdown-item">Today</a></li>
+                                <li><a href="#" class="dropdown-item">Week</a></li>
+                                <li><a href="#" class="dropdown-item active">Month</a></li>
+                                <li><a href="#" class="dropdown-item">This Year</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="card-body" id="top-5-scroll">
+                        <ul class="list-unstyled list-unstyled-border">
+                            <li class="media">
+                                <img class="mr-3 rounded" width="55" src="assets/img/products/product-3-50.png"
+                                    alt="product">
+                                <div class="media-body">
+                                    <div class="float-right">
+                                        <div class="font-weight-600 text-muted text-small">86 Sales</div>
+                                    </div>
+                                    <div class="media-title">oPhone S9 Limited</div>
+                                    <div class="mt-1">
+                                        <div class="budget-price">
+                                            <div class="budget-price-square bg-primary" data-width="64%"></div>
+                                            <div class="budget-price-label">$68,714</div>
+                                        </div>
+                                        <div class="budget-price">
+                                            <div class="budget-price-square bg-danger" data-width="43%"></div>
+                                            <div class="budget-price-label">$38,700</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="card-footer pt-3 d-flex justify-content-center">
+                        <div class="budget-price justify-content-center">
+                            <div class="budget-price-square bg-primary" data-width="20"></div>
+                            <div class="budget-price-label">Selling Price</div>
+                        </div>
+                        <div class="budget-price justify-content-center">
+                            <div class="budget-price-square bg-danger" data-width="20"></div>
+                            <div class="budget-price-label">Budget Price</div>
                         </div>
                     </div>
                 </div>

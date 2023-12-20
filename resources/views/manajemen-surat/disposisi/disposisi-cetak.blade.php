d510ad6721808aa26a7da831cade74a9e47baf10
fadli154 <fadlihifziansyah154@gmail.com>
2023-12-20 15:09:41 +0700

    Fiture: Template cetak disposisi

--- resources/views/manajemen-surat/disposisi/disposisi-cetak.blade.php
+++ resources/views/manajemen-surat/disposisi/disposisi-cetak.blade.php
@@ -8,50 +8,198 @@
     <title>PDF Disposisi {{ $dataDisposisi->pengajuan->nomor_agenda }} - {{ $dataDisposisi->tanggal_disposisi }}</title>
 
     <style>
+        @import url("https://fonts.googleapis.com/css2?family=Inter&display=swap");
+
+        body {
+            font-family: "Inter", sans-serif;
+            font-size: 14px;
+            line-height: 1.5;
+        }
+
+        p {
+            font-size: .9rem;
+
+        }
+
+        .text-bold {
+            font-weight: bold;
+        }
+
+        .border-bottom-dotted {
+            border-bottom: 1px dotted black;
+        }
+
+        .title-disposisi {
+            text-decoration: underline;
+            text-underline-offset: 1em;
+            margin-bottom: 45px;
+        }
+
         .container {
-            display: grid;
-            grid-template-areas:
-                "surat pengajuan"
-                "disposisi disposisi";
+            border: 2px solid black;
         }
 
         .surat {
-            grid-area: surat;
+            width: 315px;
+            padding: 10px 20px;
+            float: left;
+            border-right: 2px solid black;
         }
 
         .pengajuan {
-            grid-area: pengajuan;
+            width: 315px;
+            padding: 10px 20px;
+            float: right;
         }
 
         .disposisi {
-            grid-area: disposisi;
+            width: 100%;
+            padding: 20px;
+            border-top: 2px solid black;
+        }
+
+        .sifat-status {
+            width: 290px;
+            padding: 5px 15px;
+            float: left;
+        }
+
+        .sifat-disposisi {
+            width: 120px;
+            float: right;
+            text-align: center
+        }
+
+        .status-disposisi {
+            width: 140px;
+            float: left;
+            padding-right: 20px;
+            text-align: center
+        }
+
+        .diteruskan {
+            width: 300px;
+            padding: 5px 15px;
+            float: right;
+        }
+
+        .catatan-disposisi {
+            margin-top: 20px;
+            width: 100%;
+            padding: 5px 15px;
+
+        }
+
+        .ttd {
+            margin-top: 30px;
+        }
+
+        .catatan {
+            width: 290px;
+            padding: 5px 15px;
+            line-height: 0.7;
+            float: left;
+            padding-top: 65px;
+        }
+
+        .catatan .small {
+            line-height: 1.1;
+            font-size: .7rem !important;
+        }
+
+        .ttd-disposisi {
+            width: 290px;
+            float: right;
+        }
+
+        .ttd-disposisi .tanggal-tdd {
+            line-height: 1.1;
+        }
+
+        .ttd-disposisi .tanggal-tdd p {
+            font-size: .8rem !important;
         }
     </style>
 </head>
 
 <body>
+    <h2 class="title-disposisi">Diposisi Surat</h2>
     <div class="container">
-        <section class="surat">
-            <input type="text" value="{{ $dataDisposisi->pengajuan->surat->nomor_surat }}" id="nomor_agenda">
-        </section>
-        <section class="pengajuan">
-            <input type="text" value="{{ $dataDisposisi->pengajuan->nomor_agenda }}" id="nomor_agenda">
-        </section>
-        <section class="disposisi">
-            <input type="text" value="{{ $dataDisposisi->pengajuan->nomor_agenda }}" id="nomor_agenda">
-        </section>
+        <div class="d-flex justify-content-evenly">
+            <div class="surat-pengajuan">
+                <div class="surat">
+                    <p>No &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: {!! formatCetak($dataDisposisi->pengajuan->surat->nomor_surat) !!}</p>
+                    <p>Dari &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                        {!! formatCetak($dataDisposisi->pengajuan->surat->instansi->nama_instansi) !!}</p>
+                    <p>Tanggal &nbsp;&nbsp;:
+                        {!! formatCetak(date('d-F-Y', strtotime($dataDisposisi->pengajuan->surat->tanggal_surat))) !!}</p>
+                    <p>Perihal &nbsp;&nbsp;&nbsp;&nbsp;: {!! formatCetak($dataDisposisi->pengajuan->surat->isi_surat) !!}</p>
+                </div>
+                <div class="pengajuan">
+                    <p>No. Agenda&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                        {!! formatCetak($dataDisposisi->pengajuan->nomor_agenda) !!}
+                    </p>
+                    <p>Tanggal Terima&nbsp;&nbsp;&nbsp;&nbsp;:
+                        {!! formatCetak(date('d-F-Y', strtotime($dataDisposisi->pengajuan->tanggal_terima))) !!}
+                    </p>
+                    <p>Kepada&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:
+                        {!! formatCetak($dataDisposisi->penerima->posisiJabatan->nama_posisi_jabatan) !!}</p>
+                </div>
+            </div>
+        </div>
+        <div class="disposisi">
+            <div class="sifat-status">
+                <div class="status-disposisi">
+                    <p class="text-bold">Status Disposisi</p>
+                    <p style="text-align: center">
+                        ({{ convertDisposisiField($dataDisposisi->status_disposisi, 'status') }})
+                    </p>
+                </div>
+                <div class="sifat-disposisi">
+                    <p class="text-bold">Sifat Disposisi</p>
+                    <p style="text-align: center">
+                        ({{ convertDisposisiField($dataDisposisi->sifat_disposisi, 'sifat') }})
+                    </p>
+                </div>
+            </div>
+            <div class="diteruskan">
+                <p class="text-bold">Diteruskan Kepada : </p>
+                <p class="border-bottom-dotted">1. {{ $dataDisposisi->posisiJabatan->nama_posisi_jabatan }}
+                </p>
+            </div>
+            <div class="catatan-disposisi">
+                <p class="text-bold">Pesan/Catatan Disposisi</p>
+                <p class="border-bottom-dotted">{{ $dataDisposisi->catatan_disposisi }}</p>
+            </div>
+            <div class="ttd">
+                <div class="catatan">
+                    <p>Catatan: </p>
+                    <p class="small">
+                        Setelah dipelajari segera dikembalikan<br>
+                        ke Bagian Persuratan<br>
+                    </p>
+                </div>
+                <div class="ttd-disposisi">
+                    <div class="tanggal-tdd">
+                        <p class="small">Tangerang, <span>...........................</span>
+                            <br>
+                            Plt. Kepala {{ $dataDisposisi->pengajuan->surat->instansi->nama_instansi }}
+                        </p>
+                        <br>
+                        <br>
+                        <br>
+                        <p class="small">
+                            <span class="text-bold" style="text-decoration: underline">
+                                {{ $dataDisposisi->pengajuan->surat->user->nama }}
+                            </span>
+                            <br>
+                            NIP. {{ $dataDisposisi->pengajuan->surat->user->nip }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-
-
-    <div style="page-break-after: always"></div>
-
-    <embed src="{{ public_path('document_save/' . $dataDisposisi->pengajuan->surat->scan_dokumen) }}" width="500"
-        height="375" type="application/pdf">
-
 </body>
-
-<script>
-    window.print();
-</script>
 
 </html>

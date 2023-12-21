d36a6ca6baf099b55bc034bdb66fcabdbb52c26b
Your Name <you@example.com>
2023-12-21 14:38:38 +0700

    Add : New fields for Instansi

--- database/migrations/2023_10_24_081041_create_instansi_table.php
+++ database/migrations/2023_10_24_081041_create_instansi_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
             $table->string('nama_instansi', 50);
             $table->string('alamat_instansi', 150);
             $table->string('nomor_telpon', 13);
+            $table->string('email', 225)->unique();
+            $table->string('foto_instansi', 225)->nullable();
             $table->timestamps();
         });
     }
@@ -25,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('perusahaan');
+        Schema::dropIfExists('instansi');
     }
 };

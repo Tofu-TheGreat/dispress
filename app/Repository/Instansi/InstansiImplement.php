ca524b7b5bc7ca88d2a310d38464da40f0ef090b
Your Name <you@example.com>
2023-12-21 14:38:53 +0700

    configure new field in instansi

--- app/Repository/Instansi/InstansiImplement.php
+++ app/Repository/Instansi/InstansiImplement.php
@@ -4,6 +4,7 @@ namespace App\Repository\Instansi;
 
 use App\Models\Instansi;
 use App\Repository\Instansi\InstansiRepository;
+use Intervention\Image\Facades\Image;
 
 class InstansiImplement implements InstansiRepository
 {
@@ -18,11 +19,30 @@ class InstansiImplement implements InstansiRepository
     }
     public function store($data)
     {
-        $this->instansi->create([
-            'nama_instansi' => $data->nama_instansi,
-            'alamat_instansi' => $data->alamat_instansi,
-            'nomor_telpon' => $data->nomor_telpon,
-        ]);
+        if ($data->hasFile('foto_instansi')) {
+            $image = $data->foto_instansi;
+            $nama_foto = time() . '.' . $image->extension();
+            $destinationPath = public_path('/image_save');
+            $imgFile = Image::make($image->getRealPath());
+            $imgFile->resize(1200, 1200, function ($constraint) {
+                $constraint->upsize();
+            })->save($destinationPath . '/' . $nama_foto);
+            $data->foto_instansi->move(public_path('image_save'), $nama_foto);
+            $this->instansi->create([
+                'nama_instansi' => $data->nama_instansi,
+                'alamat_instansi' => $data->alamat_instansi,
+                'nomor_telpon' => $data->nomor_telpon,
+                'email' => $data->email,
+                'foto_instansi' => $nama_foto
+            ]);
+        } else {
+            $this->instansi->create([
+                'nama_instansi' => $data->nama_instansi,
+                'alamat_instansi' => $data->alamat_instansi,
+                'nomor_telpon' => $data->nomor_telpon,
+                'email' => $data->email,
+            ]);
+        }
     }
     public function show(string $id)
     {
@@ -34,11 +54,36 @@ class InstansiImplement implements InstansiRepository
     }
     public function update($data, string $id)
     {
-        $this->instansi->where('id_instansi', $id)->update([
-            'nama_instansi' => $data->nama_instansi,
-            'alamat_instansi' => $data->alamat_instansi,
-            'nomor_telpon' => $data->nomor_telpon,
-        ]);
+        if ($data->hasFile('foto_user')) {
+            $instansi = $this->instansi->where('id_instansi', $id)->first();
+            if ($instansi->foto_instansi != null) {
+                $fotoPath = public_path('image_save/') . $instansi->foto_instansi;
+                if (file_exists($fotoPath)) {
+                    unlink($fotoPath);
+                }
+            }
+            $image = $data->foto_instansi;
+            $nama_foto = time() . '.' . $image->extension();
+            $destinationPath = public_path('/image_save');
+            $imgFile = Image::make($image->getRealPath());
+            $imgFile->fit(1200, 1200, function ($constraint) {
+                $constraint->upsize();
+            })->save($destinationPath . '/' . $nama_foto);
+            $this->instansi->where('id_instansi', $id)->update([
+                'nama_instansi' => $data->nama_instansi,
+                'alamat_instansi' => $data->alamat_instansi,
+                'nomor_telpon' => $data->nomor_telpon,
+                'email' => $data->email,
+                'foto_instansi' => $nama_foto
+            ]);
+        } else {
+            $this->instansi->where('id_instansi', $id)->update([
+                'nama_instansi' => $data->nama_instansi,
+                'alamat_instansi' => $data->alamat_instansi,
+                'nomor_telpon' => $data->nomor_telpon,
+                'email' => $data->email,
+            ]);
+        }
     }
     public function destroy(string $id)
     {

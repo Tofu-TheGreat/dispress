a005647fabf9b838053355d2622e45a7f91818ff
Your Name <you@example.com>
2023-12-23 12:34:23 +0700

    Make new request for web settinng register

---
+++ app/Http/Requests/RegWebSettingRequest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegWebSettingRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nama_instansi' => 'required',
+            'nomor_telpon' => 'required|min:12|max:13|unique:instansi,nomor_telpon,' . $this->input('id_instansi') . ',id_instansi',
+            'alamat_instansi' => 'required|min:5|max:150|',
+            'email' => 'required|email|unique:instansi,email,' . $this->input('id_instansi') . ',id_instansi',
+            'id_ketua' => 'required|exists:users,id_user',
+            'default_logo' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'nama_instansi.required' => 'Nama instansi tidak boleh kosong!',
+            'nomor_telpon.required' => 'Harap masukkan nomor telepon.',
+            'nomor_telpon.min' => 'Nomor telepon tidak boleh kurang dari 12',
+            'nomor_telpon.max' => 'Nomor telepon tidak boleh lebih dari 13',
+            'nomor_telpon.unique' => 'Harap Masukkan nomor telpon yang berbeda',
+            'alamat_instansi.required' => 'Alamat instansi tidak boleh kosong!',
+            'alamat_instansi.min' => 'Alamat instansi tidak boleh kurang dari 5',
+            'alamat_instansi.max' => 'Alamat instansi tidak boleh lebih dari 150',
+            'email.required' => 'Email instansi tidak boleh kosong!',
+            'email.email' => 'Harap masukkan alamat email yang valid.',
+            'email.unique' => 'Harap Masukkan email yang berbeda',
+            'id_ketua.required' => 'Kolom ketua wajib diisi.',
+            'id_ketua.exists' => 'Ketua yang dipilih tidak valid.',
+            'default_logo.image' => 'Logo default harus berupa gambar.',
+            'default_logo.mimes' => 'Logo default harus berupa file dengan tipe: jpeg, png, jpg, gif.',
+            'default_logo.max' => 'Logo default tidak boleh lebih dari 2 megabyte.',
+        ];
+    }
+}

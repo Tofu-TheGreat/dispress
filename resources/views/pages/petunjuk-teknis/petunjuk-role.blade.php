3e544f643eb7186ea67f28d7330318caf793a6c4
fadli154 <fadlihifziansyah154@gmail.com>
2024-01-05 21:18:04 +0700

    Refactor: fill content

--- resources/views/pages/petunjuk-teknis/petunjuk-role.blade.php
+++ resources/views/pages/petunjuk-teknis/petunjuk-role.blade.php
@@ -39,61 +39,84 @@
                                 </section>
                                 <section class="section-petunjuk mt-4" id="admin" data-scroll-index="0">
                                     <h5 class="title-page-petunjuk mb-0"><i class="bi bi-person-fill-gear"></i>
-                                        Rolen Admin</h5>
+                                        Role Admin</h5>
 
-                                    <p class="text-justify">Role Admin mempunyai tugas yaitu :
+                                    <p class="text-justify">Role Admin mempunyai hak akses yaitu :
                                     </p>
 
+                                    <div class="custom-block-info mb-4">
+                                        <p class="custom-block-title mt-1 mb-50">Info</p>
+                                        <p class="custom-block-text">
+                                            CRUD Adalah singkatan (Cread, Read, Update, Delete) <br> buat, baca, perbarui,
+                                            dan
+                                            hapus data dari interface anda.
+                                        </p>
+                                    </div>
+
                                     <ol class="list-rekomendasi-perangkat">
-                                        <li>Komputer/PC</li>
-                                        <li>Tablet</li>
-                                        <li>Smartphone</li>
-                                        <li>Laptop</li>
-                                        <li>Perangkat Lainnya</li>
+                                        <li>CRUD Posisi Jabatan</li>
+                                        <li>CRUD User</li>
+                                        <li>CRUD Instansi</li>
+                                        <li>CRUD Nomor Klasifikasi</li>
+                                        <li>CRUD Surat Masuk</li>
+                                        <li>CRUD Pengajuan Disposisi</li>
+                                        <li>CRUD Disposisi (Mendisposisikan)</li>
+                                        <li>CRUD Surat Keluar</li>
+                                        <li>CRUD Surat Tugas</li>
                                     </ol>
 
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quisquam obcaecati
-                                        voluptatibus dignissimos! Deleniti qui non placeat quod repudiandae. Dicta!</p>
+                                    <p>
+                                        Role admin memiliki hak akses yang bisa mengakses semua fitur di aplikasi.
+                                    </p>
                                 </section>
                                 <section class="section-petunjuk mt-4" id="officer" data-scroll-index="1">
                                     <h5 class="title-page-petunjuk mb-0"><i class="bi bi-person-fill-lock"></i>
-                                        Rekomendasi Officer</h5>
-                                    <p class="text-justify">Role Officer mempunyai tugas yaitu :
+                                        Role Officer</h5>
+                                    <p class="text-justify">Role Officer mempunyai hak akses yaitu :
                                     </p>
 
                                     <ol class="list-rekomendasi-perangkat">
-                                        <li>Komputer/PC</li>
-                                        <li>Tablet</li>
-                                        <li>Smartphone</li>
-                                        <li>Laptop</li>
-                                        <li>Perangkat Lainnya</li>
+                                        <li>CRUD Posisi Jabatan</li>
+                                        <li>Read Users</li>
+                                        <li>CRUD Instansi</li>
+                                        <li>CRUD Nomor Klasifikasi</li>
+                                        <li>CRUD Surat Masuk</li>
+                                        <li>CRUD Pengajuan Disposisi (Yang dia ajukan)</li>
+                                        <li>Read Disposisi</li>
+                                        <li>Read Surat Keluar</li>
+                                        <li>Read Surat Tugas</li>
                                     </ol>
 
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quisquam obcaecati
-                                        voluptatibus dignissimos! Deleniti qui non placeat quod repudiandae. Dicta!</p>
+                                    <p>
+                                        Role officer memiliki hak akses yang bisa mengakses sebagian fitur di aplikasi.
+                                    </p>
                                 </section>
                                 <section class="section-petunjuk mt-4" id="staff" data-scroll-index="2">
                                     <h5 class="title-page-petunjuk mb-0"><i class="bi bi-person-fill"></i>
-                                        Staff Web</h5>
-                                    <p class="text-justify">Role Staff mempunyai tugas yaitu :
+                                        Role Staff </h5>
+                                    <p class="text-justify">Role Staff mempunyai hak akses yaitu :
                                     </p>
 
                                     <ol class="list-rekomendasi-perangkat">
-                                        <li>Komputer/PC</li>
-                                        <li>Tablet</li>
-                                        <li>Smartphone</li>
-                                        <li>Laptop</li>
-                                        <li>Perangkat Lainnya</li>
+                                        <li>Read Posisi Jabatan</li>
+                                        <li>Read Users</li>
+                                        <li>Read Instansi</li>
+                                        <li>Read Nomor Klasifikasi</li>
+                                        <li>Read Surat Masuk</li>
+                                        <li>Read Disposisi</li>
+                                        <li>Read Surat Keluar</li>
+                                        <li>Read Surat Tugas</li>
                                     </ol>
 
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quisquam obcaecati
-                                        voluptatibus dignissimos! Deleniti qui non placeat quod repudiandae. Dicta!</p>
+                                    <p>
+                                        Role staff hanya memiliki hak akses untuk melihat detail data di aplikasi.
+                                    </p>
                                     <div class="custom-block-warning ">
                                         <p class="custom-block-title mt-1 mb-50">Lanjutkan Petunjuk</p>
-                                        <a class="btn btn-sm btn-success mb-2" href="/petunjuk-teknis/registrasi">Lanjutkan
+                                        <a class="btn btn-sm btn-success mb-2" href="/petunjuk-teknis/penggunaan">Lanjutkan
                                             <i class="bi bi-arrow-right-circle-fill ms-2"></i></a>
-                                        <p class="custom-block-text">Klik untuk melanjutkan membaca petunjuk teknis -
-                                            Petunjuk registrasi
+                                        <p class="custom-block-text">Klik untuk melanjutkan membaca petunjuk role -
+                                            Petunjuk Penggunaan
                                         </p>
                                     </div>
                                 </section>
